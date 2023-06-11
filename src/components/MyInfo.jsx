3841eaa6df7a7536e7da31f4b88d7389a04967d5
MikoreVikt <136119613+MrTkachenkoA@users.noreply.github.com>
2023-06-11 16:04:57 +0300

    adds start files

---
+++ src/components/MyInfo.jsx
@@ -0,0 +1,13 @@
+export const MyInfo = () => {
+  return (
+    <div>
+      <h1>Tkachenko Anton</h1>
+      <p>Student of VNTU</p>
+      <ul>
+        <li>Cappadocia</li>
+        <li>Paris</li>
+        <li>Rome</li>
+      </ul>
+    </div>
+  );
+};
